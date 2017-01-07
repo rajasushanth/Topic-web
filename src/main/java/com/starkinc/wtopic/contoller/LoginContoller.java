0babeb61eba993066909c3c85a39d9ccb5df0187
sushneo <sush.neo@gmail.com>
2017-01-07 23:33:46 +0530

    Login Controller updated

--- src/main/java/com/starkinc/wtopic/contoller/LoginContoller.java
+++ src/main/java/com/starkinc/wtopic/contoller/LoginContoller.java
@@ -1,5 +1,17 @@
 package com.starkinc.wtopic.contoller;
 
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(name="/login", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 public class LoginContoller {
+	
+	@GetMapping("")
+	public String login(){
+		return null;
+	}
 
 }
