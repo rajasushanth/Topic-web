71aead9b71a50151a58d9fd6c21c532c9e037f68
sushneo <sush.neo@gmail.com>
2017-01-07 15:08:53 +0530

    Initial setup

---
+++ src/main/java/com/starkinc/wtopic/TopicWebApplication.java
@@ -0,0 +1,12 @@
+package com.starkinc.wtopic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicWebApplication.class, args);
+	}
+}
